28b1b4312221f108322003a5bdf1f0623b3fcdc8
datnguyen79198 <datnguyen79198@gmail.com>
2022-07-29 17:18:50 +0300

    init spring boot app

--- Scheduler/src/main/java/com/github/datnguyenzzz/App.java
+++ Scheduler/src/main/java/com/github/datnguyenzzz/App.java
@@ -1,13 +1,18 @@
 package com.github.datnguyenzzz;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
+
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
---
+++ Scheduler/src/main/java/com/github/datnguyenzzz/Controller/WebController.java
@@ -0,0 +1,15 @@
+package com.github.datnguyenzzz.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class WebController {
+    
+    @GetMapping
+    public String index() {
+        return "Okay!!";
+    }
+}
