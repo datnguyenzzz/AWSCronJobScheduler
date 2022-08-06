2098cbddae590737bf034dca80d3b0c98d2e33f8
datnguyen79198 <datnguyen79198@gmail.com>
2022-08-06 15:52:33 +0300

    init health check action

--- Scheduler/src/main/java/com/github/datnguyenzzz/App.java
+++ Scheduler/src/main/java/com/github/datnguyenzzz/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.github.datnguyenzzz.Actions.HealthCheck;
 import com.github.datnguyenzzz.Actions.SchedulerExecution;
 
 /**
@@ -18,6 +19,9 @@ public class App implements CommandLineRunner {
     @Autowired
     private SchedulerExecution schedulerExecution;
 
+    @Autowired
+    private HealthCheck healthCheck;
+
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
     }
@@ -25,5 +29,6 @@ public class App implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         this.schedulerExecution.start();
+        this.healthCheck.start();
     }
 }
